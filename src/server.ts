c8d917e271d290faa00a0247372a917c43f78626
josevitormorais <josevitormorais7@gmail.com>
2021-03-22 21:29:45 -0300

    add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.listen(3333, () => {
-  console.log("Server Starting in port 3333");
+  console.log('Server Starting in port 3333');
 });
